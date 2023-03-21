46e2aa8796d71e87871666554abdf739caf8752f
t-murii <tmuriithi707@gmail.com>
2023-03-20 18:17:26 -0700

    #101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: 1.
+ */
+int main(void)
+{
+	char message1[] = "and that piece of art is useful";
+	char message2[]	= "- Dora Korpar, 2015-10-19\n";
+
+	strcat(message1, message2)
+	int messagesize = sizeof(message) - 1;
+
+	write(2, message1, messagesize);
+	return (1);
+}
+

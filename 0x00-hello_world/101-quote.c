4bde3b8b11a7714a2330fddac8afededc278fcca
t-murii <tmuriithi707@gmail.com>
2023-03-20 18:28:19 -0700

    #101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,12 +8,9 @@
  */
 int main(void)
 {
-	char message1[] = "and that piece of art is useful";
-	char message2[]	= "- Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	strcat(message1, message2);
-
-	write(2, message1, sizeof(message1) - 1);
+	write(2, message, sizeof(message) - 1);
 	return (1);
 }
 

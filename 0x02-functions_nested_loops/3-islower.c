44d5c29f8fa2120e561304df131960d1533da644
t-murii <tmuriithi707@gmail.com>
2023-03-22 02:25:42 -0700

    Bulk commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Checks for lowercase characters only
- *@c: variable to check, variable type char
+ * @c: variable to check, variable type char
  * Return: 1 if c is lowercase, 0 if not
  */
 int _islower(int c)

7e83fc88648ffdecd83719ecc8095788631ce97e
t-murii <tmuriithi707@gmail.com>
2023-04-03 10:39:39 -0700

    First commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 /**
- *
- *memset - a function that fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * _memset - a function that fills the first n bytes of
+ * the memory area pointed to by s with the constant byte b
  *
  * @s : varible type char for the pointer of the block of memory to be filled
  * @b : the variable to be filled in the memory area
  * @n : variable type unsigned int of the number of bytes to be filled
  *
- * Return : the pointer to the memory area @s
+ * Return: the pointer to the memory area @s
  *
  */
 char *_memset(char *s, char b, unsigned int n)

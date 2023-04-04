4cb52f4707025efcc9d5d9504b8c6847c079fc78
t-murii <tmuriithi707@gmail.com>
2023-04-03 17:26:13 -0700

    bulk commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,10 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int x = 0; x < 8; x++)
+	int x, y;
+
+	for (x = 0; x < 8; x++)
 	{
 
-		for (int y = 0; y < 8; y++)
+		for (y = 0; y < 8; y++)
 		{
 
 			_putchar (a[x][y]);

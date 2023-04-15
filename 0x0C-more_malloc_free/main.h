c3ade373d9ee709389731a93dc66ec7c4a0e87cd
t-murii <tmuriithi707@gmail.com>
2023-04-14 19:42:48 -0700

    More malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
